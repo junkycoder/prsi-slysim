d7fd629be21b6f8596d5ef8b375477f0e385b046
Dan Hromada <hromada.dan@gmail.com>
2021-12-18 07:14:12 +0100

    Remove root npm package but init firebase
    ## Firebase
    
    * Firestore
    * Functions
    * Auth
    * Hosting
    
    ## Install and development
    ```sh
    nvm use # .nvmrc = v14
    npm install
    npm i -g firebase-tools
    firebase emulators:start
    ```
    
    ## GitHub workflows
    
    Deploy hosting
    * push to main
    * rebase PR to main

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require("firebase-functions");
+import functions from "firebase-functions";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+export const userCreatesGame = functions
+  .region(config.firebase.region)
+  .firestore.document("users/{userId}/games/{gameId}")
+  .onCreated((snapshot, context) => {
+    console.log("userCreatesGame", snapshot, context);
+  });
