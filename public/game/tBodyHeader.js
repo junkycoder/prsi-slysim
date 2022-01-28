f1847dd4cd852d94e4584394d9f1fb316d292182
Dan Hromada <hromada.dan@gmail.com>
2022-01-28 18:52:15 +0100

    reorganise sourcode

---
+++ public/game/tBodyHeader.js
@@ -0,0 +1,34 @@
+import { html } from "https://unpkg.com/lit-html@2.1.1/lit-html.js?module";
+
+const gameStatusToTitle = (status) => {
+  switch (status) {
+    case 0:
+      return "Hra ještě nezačala";
+    default:
+      return "Zatím nejsou data";
+  }
+};
+
+export default function BodyHeader({
+  gameStatus = "",
+  playerName = "",
+  maxPlayers,
+  playersCount,
+} = {}) {
+  return html`
+    <header>
+      <h1>${gameStatusToTitle(gameStatus)}</h1>
+      <!-- <h1>Jarda míchá</h1> -->
+      <!-- <h1>Jarda míchá</h1> -->
+      <!-- <h1>Pepa táhne</h1> -->
+      <!-- <h1>Franta líže dvě karty</h1> -->
+      <p>
+      ${playerName ?
+      html`
+        U stolu sedí ${playerName} <br />
+        a ${playersCount - 1 || "nikdo"} další. Maximalní počet hráčů je
+        ${maxPlayers}.` : "Zde by se zobrazí základní informace vytvořené hře."}
+        </p>
+    </header>
+  `;
+}
--- public/game/tDocumentBody.js
+++ public/game/tDocumentBody.js
@@ -1,7 +1,7 @@
 import { html } from "https://unpkg.com/lit-html@2.1.1/lit-html.js?module";
-import tBodyHeader from "./BodyHeader.js";
-import tTodoList from "./TodoList.js";
-import tPlayerList from "./PlayerList.js";
+import tBodyHeader from "./tBodyHeader.js";
+import tTodoList from "./tTodoList.js";
+import tPlayerList from "./tPlayerList.js";
 
 const getUserPlayer = (userId, players = []) =>
   players.find((player) => userId && player.id === userId);
@@ -25,25 +25,32 @@ export default function DocumentBody({ user, game = {} } = {}) {
         ${tPlayerList({ players: game.players, userId: user?.id })}
         <!--  -->
         <h2>Stůl</h2>
-        ${lastCard &&
-        html`<figure>${lastCard.value} ${lastCard.color}</figure>`}
+        <figure>
+          Karta na stole:
+          <strong>${lastCard ? `${lastCard.value} ${lastCard.color}` : "žádná"}</strong>
+        </figure>
         <figure>Balíček karet (${game.deck?.length})</figure>
       </section>
       <section>
         <h2>Tvoje možnosti</h2>
-        <select name="card" onchange="handleSelectedHandCardChanged(event)" disabled>
+        <button name="suffle">Zamíchat karty na stole</button>
+        <button disabled name="deal">Rozdat karty</button>
+        <button disabled name="draw">Líznout si</button>
+        <select
+          name="card"
+          onchange="handleSelectedHandCardChanged(event)"
+          disabled
+        >
           ${userPlayer?.cards.map(
             ({ id, value, color }) =>
               html`<option value="${id}">${value} ${color}</option>`
           )}
-          ${!userPlayer?.cards.length && html`<option disabled selected>žádné karty v ruce</option>`}
+          ${!userPlayer?.cards.length &&
+          html`<option disabled selected>žádné karty v ruce</option>`}
         </select>
-        <button name="suffle">Zamíchat karty</button>
-        <button disabled name="deal">Rozdat karty</button>
         <button disabled name="move">Táhnout kartu</button>
-        <button disabled name="draw">Líznout si</button>
         <button disabled name="stay">Stát</button>
-        <button name="leave">Vzdát se a opustit hru</button>
+        <button name="leave">Opustit hru</button>
       </section>
       <!-- ${tTodoList()} -->
     </main>
