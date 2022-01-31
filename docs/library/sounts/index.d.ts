7f58a09e159a45bce921ed2f806f1603fc3598e1
Dan Hromada <hromada.dan@gmail.com>
2022-01-31 08:56:27 +0100

    Remove google pages example copy
    use workin prod instead https://prsi-slysim.web.app/

--- docs/library/sounts/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./createAudioContext";
-export * from "./SoundListener";
-export * from "./SoundPlayer";
-export * from "./SoundSource";
--- docs/library/sounts/index.js
+++
@@ -1,6 +0,0 @@
-
-export { createAudioContext } from "./createAudioContext.js";
-export { SoundListener, createSoundListener } from "./SoundListener.js";
-export { SoundPlayer } from "./SoundPlayer.js";
-export { SoundSource } from "./SoundSource.js";
-
