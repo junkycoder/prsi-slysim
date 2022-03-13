954a6811a8c215994eec8c534a60611c7d14f44c
Dan Hromada <hromada.dan@gmail.com>
2022-03-13 18:12:06 +0100

    Move stats lambda under game.js

--- functions/game.js
+++ functions/game.js
@@ -242,6 +242,56 @@ export const makeCpuMove = functions
   });
 
 /**
+ * Aggregate and store statistic
+ */
+export const stats = functions
+  .region("europe-west1")
+  .pubsub.schedule("every 60 minutes")
+  .onRun(async () => {
+    const db = admin.firestore();
+    const { docs: games } = await db
+      .collection("play/private/game")
+      .orderBy("createdAt", "asc")
+      .get();
+    const stats = {
+      games: games.length,
+      players: 0,
+      winner: "",
+      moves: 0,
+    };
+
+    const leaderboard = {};
+
+    for (let game of games) {
+      const { players = [], moves = [] } = game.data();
+      stats.moves += moves.length;
+
+      for (let player of players.filter(({ cpu }) => !cpu)) {
+        const score = leaderboard[player.id];
+        if (score) {
+          leaderboard[player.id] = {
+            name: player.name,
+            count: score.count + 1,
+          };
+        } else {
+          leaderboard[player.id] = {
+            name: player.name,
+            count: 1,
+          };
+        }
+      }
+    }
+
+    stats.players += Object.keys(leaderboard).length;
+    const [leader] = Object.values(leaderboard).sort(
+      (a, b) => b.count - a.count
+    );
+    stats.winner = leader;
+
+    await db.doc("public/stats").set(stats, { merge: true });
+  });
+
+/**
  * function that keeps game copies in sync.
  */
 function makeGameCopies(game, { db, batch }) {
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@
  * Prší backend functions
  */
 import admin from "firebase-admin";
-import functions from "firebase-functions";
 
 admin.initializeApp();
 
@@ -10,52 +9,7 @@ export {
   create as createGame,
   join as joinGame,
   move as gameMove,
-  makeCpuMove
+  makeCpuMove,
+  stats,
 } from "./game.js";
 
-export const stats = functions
-  .region("europe-west1")
-  .pubsub.schedule("every 60 minutes")
-  .onRun(async () => {
-    const db = admin.firestore();
-    const { docs: games } = await db
-      .collection("play/private/game")
-      .orderBy("createdAt", "asc")
-      .get();
-    const stats = {
-      games: games.length,
-      players: 0,
-      winner: "",
-      moves: 0,
-    };
-
-    const leaderboard = {};
-
-    for (let game of games) {
-      const { players = [], moves = [] } = game.data();
-      stats.moves += moves.length;
-
-      for (let player of players.filter(({ cpu }) => !cpu)) {
-        const score = leaderboard[player.id];
-        if (score) {
-          leaderboard[player.id] = {
-            name: player.name,
-            count: score.count + 1,
-          };
-        } else {
-          leaderboard[player.id] = {
-            name: player.name,
-            count: 1,
-          };
-        }
-      }
-    }
-
-    stats.players += Object.keys(leaderboard).length;
-    const [leader] = Object.values(leaderboard).sort(
-      (a, b) => b.count - a.count
-    );
-    stats.winner = leader;
-
-    await db.doc("public/stats").set(stats, { merge: true });
-  });
