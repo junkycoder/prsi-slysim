8ea15b683ad8b94552a7e6c7b5c0ff27ea502ae0
Dan Hromada <hromada.dan@gmail.com>
2022-03-25 08:50:52 +0100

    Add npm module due to capacitor 😭

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-firebase emulators:start --export-on-exit --import dump
+
+
+firebase emulators:start --export-on-exit --import dump || echo 'Did you ran `npm install firebase-tools -g`?'
